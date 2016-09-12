cd6f08ea1b3470d8558d5013f25a8c2100280f38
jim <jim@magicare.me>
2016-09-12 19:23:58 +0800

    up

---
+++ App/Admin/Public/index.php
@@ -0,0 +1,4 @@
+<?php
+define('TSF_PATH',dirname(dirname(dirname(dirname(__FILE__)))).'/TSF/');
+require_once (TSF_PATH.'TSF.php');
+TSF::init();
---
+++ Common/Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'db' =>[
+        'host' =>'',
+        'port' => '',
+        'database'=>'',
+        'user' => '',
+        'password' => ''
+    ]
+];
